a6fc7128b6a6c1a081d065bdf0c9129d993b8d10
Moh7ven <mohamedsangare0107@gmail.com>
2022-06-09 17:38:59 -0700

    0th task isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * _isupper - check if the letter is upper
+  * @x: the number to be checked
+  * 
+  * Return: 1for upper letter 0 for anny else
+  */
+int _isupper(int x)
+{
+	if (x >= 65 && x <= 90)
+	{
+		return (i)
+	}
+	
+	return (0);
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This Repositery contains my ALX PROJECT on more_functions_nested_loops
